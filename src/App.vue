3341951191f4e394b17ded8ba6ced85f36c3e3ef
Niko-Mata <zh1584789282@gmail.com>
2023-07-08 11:51:51 +0800

    install sass

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,22 @@
 
 <template>
     <div class="">
-        
+        <div class="header">
+
+        </div>
     </div>
 </template>
 
 <style scoped lang="scss">
+.box{
+    width: 100px;
+    height: 100px;
+    background: #9291f1;
 
+    .header{
+        width: 50px;
+        height: 50px;
+        background: #5818f1;
+    }
+}
 </style>
