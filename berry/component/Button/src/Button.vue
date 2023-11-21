119fe87438fa43d8e968df969757dba5fb30d3db
augustxzx <1753867820@qq.com>
2023-11-22 01:16:13 +0800

    add

--- berry/component/Button/src/Button.vue
+++ berry/component/Button/src/Button.vue
@@ -9,31 +9,51 @@ defineOptions({
   name: 'BerryButton'
 })
 
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { useNS } from "berry-ui/hooks/useNS"
 import { ButtonProps } from './Button'
 import BerryIcon from '../../Icon'
+import { convertToObject } from "typescript";
 const ns = useNS('button')
 
 const props = defineProps({ ...ButtonProps })
+console.log(props.prefixIcon)
+console.log(props.suffixIcon)
 const kls = computed(() => {
-
   return [
     ns.namespace,
     ns.m(props.type as string),
     ns.m(props.variant as string),
-    ns.m(props.status as string)
+    ns.m(props.status as string),
   ]
 })
+
+let rotateFlag = ref<boolean>(false)
+let loadingStatus = ref<boolean>(false)
+
+const clickBtn = function (): boolean {
+  if (!props.loading) {
+    return false
+  }
+  rotateFlag.value = props.loading ? props.loading : !props.loading
+  setTimeout(() => {
+    rotateFlag.value = false
+    loadingStatus.value = true
+  }, 2000)
+  return true
+}
 
 </script>
 
 <template>
-  <button ref="container" :title="ns.namespace" v-bind="$attrs" :class="kls" :type="(props.nativeType)">
-    <BerryIcon v-if="loading" name="rotate" color="rgb(15,15,15)" size="16px" :rotate="props.loading"></BerryIcon>
-    <BerryIcon v-if="!loading" :name="props.prefixIcon" color="rgb(15,15,15)" size="16px" :rotate="props.loading"></BerryIcon>
-    <slot></slot>
-    <BerryIcon v-if="!loading" :name="props.suffixIcon" color="rgb(15,15,15)" size="16px" :rotate="props.loading"></BerryIcon>
+  <button ref="container" :title="ns.namespace" v-bind="$attrs" :class="kls" :type="(props.nativeType)" @click="clickBtn">
+    <BerryIcon v-if="rotateFlag" name="rotate" color="rgb(15,15,15)" size="16px" :rotate="props.loading">
+    </BerryIcon>
+    <BerryIcon v-if="prefixIcon&&!rotateFlag" :name="prefixIcon" color="rgb(15,15,15)" size="16px" :class="loadingStatus ? 'is-loading' : ''">
+    </BerryIcon>
+    <slot class="ber-button--content"></slot>
+    <BerryIcon v-if="suffixIcon&&!rotateFlag" :name="suffixIcon" color="rgb(15,15,15)" size="16px" :class="loadingStatus ? 'is-loading' : ''">
+    </BerryIcon>
   </button>
 </template>
 
--- example/App.vue
+++ example/App.vue
@@ -9,14 +9,12 @@
 
 <template>
   <div>
-    <BerryIcon name="rotate" color="rgb(15,15,15)" size="48px" :rotate="true"></BerryIcon>
-    <BerryIcon name="rotate" color="rgb(15,15,15)" size="48px" :rotate="false"></BerryIcon>
+    
     <div style="width: 40%; display: flex;flex-direction: row;justify-content: space-around;">
-      <BerryButton type="pirmary" variant="ghost" status="round" loading>Default</BerryButton>
-      <BerryButton type="info" variant="ghost" status="round" prefixIcon="rotate">Info</BerryButton>
-      <BerryButton type="success" variant="ghost" status="round" suffixIcon="rotate">Success</BerryButton>
-      <BerryButton type="warning" variant="ghost" status="round">Warning</BerryButton>
-      <BerryButton type="error" variant="ghost" status="round">Error</BerryButton>
+
+      <BerryButton type="success" variant="s" status="round" prefixIcon="jubao" loading>Warning</BerryButton>
+      <BerryButton type="error" variant="ghost" status="round" suffixIcon="jubao" loading>Error</BerryButton>
+      <BerryButton type="info" variant="ghost" status="round" loading prefixIcon="jubao">Warning</BerryButton>
     </div>
 
   </div>
