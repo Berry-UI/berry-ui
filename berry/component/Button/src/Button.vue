06f5b1dee2238a6b3c7c42ffd432af9fd418edca
augustxzx <1753867820@qq.com>
2023-11-28 10:26:52 +0800

    update

--- berry/component/Button/src/Button.vue
+++ berry/component/Button/src/Button.vue
@@ -6,51 +6,60 @@
 <script setup lang="ts">
 import { computed, ref } from "vue";
 import { useNS } from "berry-ui/hooks/useNS"
-import { ButtonProps } from "./Button"
+import { ButtonProps, ButtonEmits } from "./Button"
 import BerryIcon from "../../Icon"
+import { emit } from "process";
 const ns = useNS("button")
 
 defineOptions({
   inheritAttrs: false,
   name: "BerryButton"
 })
+let rotateFlag = ref<boolean>(false)
+let loadingStatus = ref<boolean>(false)
 
 const props = defineProps({ ...ButtonProps })
+const emits = defineEmits({ ...ButtonEmits })
 const kls = computed(() => {
   return [
     ns.namespace,
     ns.m(props.type as string),
     ns.m(props.variant as string),
     ns.m(props.status as string),
-    ns.m(props.size as string)
+    ns.m(props.size as string),
+    ns.is(props.disabled, 'disabled')
   ]
 })
+const handleClick = (evt: MouseEvent) => {
+  if (!props.disabled) {
+    emits('click', evt)
 
-let rotateFlag = ref<boolean>(false)
-let loadingStatus = ref<boolean>(false)
-
-const clickBtn = function (): boolean {
-  if (!props.loading) {
-    return false
-  }
-  rotateFlag.value = props.loading ? props.loading : !props.loading
-  setTimeout(() => {
-    rotateFlag.value = false
-    loadingStatus.value = true
-  }, 2000)
-  return true
+    if (!props.loading) {
+      return false
+    }
+    rotateFlag.value = props.loading ? props.loading : !props.loading
+    setTimeout(() => {
+      rotateFlag.value = false
+      loadingStatus.value = true
+    }, 2000)
+    return true
+  } 
 }
-
+console.log(props)
 </script>
 
 <template>
-  <button ref="container" :title="ns.namespace" v-bind="$attrs" :class="kls" :type="(props.nativeType)" @click="clickBtn">
+  <button ref="container" :title="ns.namespace" v-bind="$attrs" :class="kls" :type="(props.nativeType)"
+    @click="handleClick($event)">
+
     <BerryIcon v-if="rotateFlag" name="rotate" color="rgb(15,15,15)" size="16px" :rotate="props.loading">
     </BerryIcon>
-    <BerryIcon v-if="prefixIcon&&!rotateFlag" :name="prefixIcon" color="rgb(15,15,15)" size="16px" :class="loadingStatus ? 'is-loading' : ''">
+    <BerryIcon v-if="prefixIcon && !rotateFlag" :name="prefixIcon" color="rgb(15,15,15)" size="16px"
+      :class="loadingStatus ? 'is-loading' : ''">
     </BerryIcon>
     <slot class="ber-button--content"></slot>
-    <BerryIcon v-if="suffixIcon&&!rotateFlag" :name="suffixIcon" color="rgb(15,15,15)" size="16px" :class="loadingStatus ? 'is-loading' : ''">
+    <BerryIcon v-if="suffixIcon && !rotateFlag" :name="suffixIcon" color="rgb(15,15,15)" size="16px"
+      :class="loadingStatus ? 'is-loading' : ''">
     </BerryIcon>
   </button>
 </template>
--- example/App.vue
+++ example/App.vue
@@ -5,12 +5,15 @@
 -->
 <script setup lang="ts">
 // import { BerryButton, BerryIcon } from 'berry-ui'
+  const cli = () => {
+    console.log(123)
+  }
 </script>
 
 <template>
   <!-- <div>
     <div style="width: 40%; display: flex;flex-direction: row;justify-content: space-around;">
-      <BerryButton type="primary" status="round" size="small" variant="ghost" suffixIcon="jubao">Default</BerryButton>
+      <BerryButton type="primary" status="round" size="small" variant="ghost" suffixIcon="jubao" @click="cli()">Default</BerryButton>
       <BerryButton type="info" variant="ghost" status="round" size="normal" prefixIcon="jubao">Default</BerryButton>
       <BerryButton type="info" variant="ghost" status="round" size="large">Default</BerryButton>
       <BerryButton type="success" status="round" size="small">Success</BerryButton>
@@ -18,7 +21,6 @@
       <BerryButton type="success" variant="ghost" status="round" size="large">Success</BerryButton> 
     </div>
   </div> -->
-  <!-- <BerryButton type="info" variant="ghost" status="round" prefixIcon="rotate" loading>Info</BerryButton> -->
   <BerryInput icon="prefix" size="small" disabled></BerryInput>
   <BerryInput icon="prefix" ></BerryInput>
   <BerryInput icon="prefix" size="large"></BerryInput>
